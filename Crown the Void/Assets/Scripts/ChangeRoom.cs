3f818adc7d177620c859ffd9cd04315f51568a0f
jacobmalone2 <143432329+jacobmalone2@users.noreply.github.com>
2025-02-22 09:08:38 -0700

    .

---
+++ Crown the Void/Assets/Scripts/ChangeRoom.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeRoom : MonoBehaviour
+{
+    public GameObject Player;
+    public string SceneName;
+    public float x, y, z;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            SceneManager.LoadScene(SceneName);
+            Player.transform.position = new Vector3(x, y, z);
+        }
+    }
+
+}
