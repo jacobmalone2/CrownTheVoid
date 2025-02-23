0c6650e73ffb6a4278a7727eec7f90f57506ebac
jacobmalone2 <143432329+jacobmalone2@users.noreply.github.com>
2025-02-23 11:43:13 -0700

    Finished level.

--- Crown the Void/Assets/Scripts/ChangeRoom.cs
+++ Crown the Void/Assets/Scripts/ChangeRoom.cs
@@ -12,7 +12,7 @@ public class ChangeRoom : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
