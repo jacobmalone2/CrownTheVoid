e6c9f46f1dc3d56ae09d36518b5d64c2abe989e5
Julian Archuleta <169094216+JulianArchuleta@users.noreply.github.com>
2025-02-23 16:56:41 -0700

    Added in YOU WIN screen

---
+++ Crown the Void/Assets/Scripts/Win.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour
+{
+    public GameObject winText;
+    public GameObject mainMenuButton;
+    public GameObject GrayOut;
+    private GameObject Player;
+    void Start()
+    {
+        Player = GameObject.FindWithTag("Player");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            winText.SetActive(true);
+            mainMenuButton.SetActive(true);
+            GrayOut.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
