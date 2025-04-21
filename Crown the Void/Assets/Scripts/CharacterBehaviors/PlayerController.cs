d48c76d2694ba9445656b5fe2ad02864acd9ce79
Julian Archuleta <169094216+JulianArchuleta@users.noreply.github.com>
2025-04-20 19:55:05 -0600

    char select

--- Crown the Void/Assets/Scripts/CharacterBehaviors/PlayerController.cs
+++ Crown the Void/Assets/Scripts/CharacterBehaviors/PlayerController.cs
@@ -742,4 +742,8 @@ public class PlayerController : MonoBehaviour
         SceneManager.LoadScene("MainMenu");
         Destroy(gameObject);
     }
+    public void CharacterSelect()
+    {
+        SceneManager.LoadScene("CharacterSelect");
+    }
 }
